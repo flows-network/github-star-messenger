3c2c7bb242aa792fa31018fa1b7d9ff5a9ccc460
Ji Chen <jaykchen@gmail.com>
2023-03-03 09:19:47 -0500

    init

---
+++ src/github-star-messenger.rs
@@ -0,0 +1,31 @@
+use github_flows::{listen_to_event, EventPayload};
+use sendgrid_flows::{send_email, Email};
+
+#[no_mangle]
+#[tokio::main(flavor = "current_thread")]
+pub async fn run() -> anyhow::Result<()> {
+    listen_to_event("jaykchen", "a-test", vec!["star"], handler).await;
+
+    Ok(())
+}
+
+async fn handler(payload: EventPayload) {
+    let your_repo_name: &str = "a-test";
+
+    if let EventPayload::UnknownEvent(e) = payload {
+        let stargazers_count = e["repository"]["stargazers_count"].as_i64().unwrap_or(-1);
+
+        let text = format!(
+            "Congratulations on your repository {your_repo_name} with {stargazers_count} stars."
+        );
+
+        if stargazers_count % 10 == 0 {
+            let email = Email {
+                to: vec![String::from("jaykchen@gmail.com")],
+                subject: String::from("Hi"),
+                content: text,
+            };
+            send_email("jaykchen@gmail.com", &email).expect("failed to send email");
+        }
+    }
+}
